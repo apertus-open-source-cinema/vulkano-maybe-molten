adf98e31191a23fecc281fbd7d73bdf8cfa297f8
Jaro Habiger <jarohabiger@googlemail.com>
2022-10-06 14:29:55 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+use std::sync::Arc;
+use vulkano::instance::{Instance, InstanceCreateInfo, InstanceCreationError, InstanceExtensions};
+
+pub trait NewMaybeMolten {
+    fn new_maybe_molten(
+        create_info: InstanceCreateInfo,
+    ) -> Result<Arc<Instance>, InstanceCreationError>;
+}
+impl NewMaybeMolten for Instance {
+    fn new_maybe_molten(
+        create_info: InstanceCreateInfo,
+    ) -> Result<Arc<Instance>, InstanceCreationError> {
+        #[cfg(not(target_os = "macos"))]
+        {
+            Instance::new(vulkano::instance::InstanceCreateInfo {
+                enabled_extensions: extensions,
+                ..create_info
+            })
+        }
+
+        #[cfg(target_os = "macos")]
+        {
+            use std::os::raw::{c_char, c_void};
+            use vulkano::instance::loader::{FunctionPointers, Loader};
+
+            let extensions = InstanceExtensions {
+                khr_surface: true,
+                mvk_macos_surface: true,
+                ..create_info.enabled_extensions
+            };
+
+            struct AshMoltenLoader {
+                static_fn: ash_molten_version::vk::StaticFn,
+            }
+            unsafe impl Loader for AshMoltenLoader {
+                fn get_instance_proc_addr(
+                    &self,
+                    instance: ash_vulkano_version::vk::Instance,
+                    name: *const c_char,
+                ) -> *const c_void {
+                    let inner_result = unsafe {
+                        self.static_fn
+                            .get_instance_proc_addr(std::mem::transmute(instance), name)
+                    };
+                    if let Some(result) = inner_result {
+                        result as *const c_void
+                    } else {
+                        0 as *const c_void
+                    }
+                }
+            }
+
+            let function_pointers: FunctionPointers<Box<dyn Loader>> =
+                FunctionPointers::new(Box::new(AshMoltenLoader {
+                    static_fn: ash_molten::load().static_fn().clone(),
+                }));
+
+            Instance::new(vulkano::instance::InstanceCreateInfo {
+                enabled_extensions: extensions,
+                function_pointers: Some(function_pointers),
+                ..create_info
+            })
+        }
+    }
+}
